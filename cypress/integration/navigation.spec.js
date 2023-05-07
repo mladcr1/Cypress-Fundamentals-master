9c317bcdae8fc954a695d7c0a497df0f19eea2be
Cristian Mlaidn <christian_mladin@yahoo.com>
2023-05-06 21:49:00 -0700

    mmmmmmmmmmmmmmmmmmmmmmm

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Navigation", () => {
-    it("should navigate to conference session pag", () => {
+    it("should navigate to conference session pagina", () => {
         cy.visit("http://localhost:1337/conference");
         cy.get("h1").contains("View Sessions").click();
         cy.url().should("include", "/session");
